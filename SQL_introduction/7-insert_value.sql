d60dc7364cadff92910509f14bb11d7e5f857f59
celestineamima <celestineamima@gmail.com>
2023-08-20 13:03:17 +0300

    My sql projjects alx

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -2,3 +2,7 @@
 -- insert a new row to the first_table
 -- values ; id = 89
 -- name = Holberton School
+
+
+INSERT INTO first_table VALUES (89,'Holberton School');
+
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- This script shuold;
+-- Create a second table 'second_table'
+-- Add multiple rows
+-- id, name, score
+-- create several records
+
+CREATE DATABASE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+--INSERTING THE RECORDS
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
